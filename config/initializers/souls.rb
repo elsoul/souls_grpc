3331d157e121430ebab7911a478be293554abb3b
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2020-11-19 21:54:14 +0100

    add base_query

--- config/initializers/souls.rb
+++ config/initializers/souls.rb
@@ -1,9 +1,11 @@
-Rails::Gke.configure do |config|
+Souls.configure do |config|
   config.project_id = "elsoul2"
-  config.app = "elquest-api"
+  config.app = "blog"
   config.network = "elsoul"
   config.machine_type = "custom-1-6656"
   config.zone = "asia-northeast1-b"
-  config.domain = "elquest-api.el-soul.com"
+  config.domain = "blog.el-soul.com"
   config.google_application_credentials = "./config/credentials.json"
+  config.strain = "api"
+  config.proto_package_name = "souls"
 end
