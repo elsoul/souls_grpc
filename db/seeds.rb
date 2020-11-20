2471547b413e1912d35f4eae5de3b5c2c3f600d9
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2020-11-20 16:53:46 +0100

    remove files

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-# Initial Data need to be saved in DB.
-# rails db:seed RAILS_ENV=development
-
-puts "created user" if User.create(
-  id: "xxxxxxx",
-  username: "POPPIN-FUMI",
-  screen_name: "ELSOUL",
-  email: "info@xxx.com"
-)
