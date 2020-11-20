8973f19aa2dba41501e80da00e618848c5954218
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2020-11-20 10:11:30 +0100

    proto path

--- app/graphql/mutations/hello_world.rb
+++ app/graphql/mutations/hello_world.rb
@@ -1,16 +1,17 @@
 module Mutations
+  # Zeitwerk で読み込みたいところ
+  require "./app/services/souls"
   class HelloWorld < BaseMutation
     field :response, String, null: false
     argument :name, String, required: false
     argument :host, String, required: false
 
-    def resolve name: "world", host: ENV["GRPC_SERVER_URL"]
-      user = !ARGV.empty? ? ARGV[0] : name
-      stub = Souls::Blog::Stub.new(host, :this_channel_is_insecure)
+    def resolve name: "SOULs", host: "localhost:50051"
       begin
-        message = stub.say_hello(Souls::HelloRequest.new(name: user)).message
+        stub = Souls::Blog::Stub.new(host, :this_channel_is_insecure)
+        message = stub.say_hello(Souls::HelloRequest.new(name: name)).message
         p "Greeting: #{message}"
-        message = stub.say_hello_again(Souls::HelloRequest.new(name: user)).message
+        message = stub.say_hello_again(Souls::HelloRequest.new(name: name)).message
         p "Greeting: #{message}"
         { response: message.to_s }
       rescue GRPC::BadStatus => e
